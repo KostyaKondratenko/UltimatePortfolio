4e34cc6dc7cfb8bac56315e9ea5e4cc9d4bc9f7d
Kostya Kondratenko <kondratenkokostya@gmail.com>
2021-01-31 19:11:28 +0200

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Kostya Kondratenko on 31.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
