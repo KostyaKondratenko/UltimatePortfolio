dabd763276e0a2210f12262785f998d78c5206c7
Kostya Kondratenko <kondratenkokostya@gmail.com>
2021-02-01 21:16:22 +0200

    Saving current tab to user defaults (SceneStorage)

---
+++ UltimatePortfolio/Constants/StorageKeys.swift
@@ -0,0 +1,12 @@
+//
+//  AppStorageKeys.swift
+//  UltimatePortfolio
+//
+//  Created by Kostya Kondratenko on 01.02.2021.
+//
+
+import Foundation
+
+struct StorageKeys {
+    static var tabKey = "selectedTabTag"
+}
---
+++ UltimatePortfolio/Constants/Tags.swift
@@ -0,0 +1,17 @@
+//
+//  Tags.swift
+//  UltimatePortfolio
+//
+//  Created by Kostya Kondratenko on 01.02.2021.
+//
+
+import SwiftUI
+
+extension HomeView {
+    static var tag: String? = "Home"
+}
+
+extension ProjectsView {
+    static var openTag: String? = "Open"
+    static var closedTag: String? = "Closed"
+}
